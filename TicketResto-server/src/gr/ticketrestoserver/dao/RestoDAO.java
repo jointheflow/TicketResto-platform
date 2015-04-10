6a63f5554a5c9a167b3830c1114b7c7622c797db
gianluca.ragnoni <gianluca.ragnoni@gmail.com>
2015-04-10 17:01:35 +0200

    First commit!

---
+++ TicketResto-server/src/gr/ticketrestoserver/dao/RestoDAO.java
@@ -0,0 +1,11 @@
+package gr.ticketrestoserver.dao;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import com.google.appengine.api.datastore.Key;
+
+
+public class RestoDAO {
+
+}
---
+++ TicketResto-server/src/gr/ticketrestoserver/entity/Payment.java
@@ -0,0 +1,105 @@
+package gr.ticketrestoserver.entity;
+
+import java.util.Date;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+
+
+@PersistenceCapable
+public class Payment {
+	
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key id;
+	
+	@Persistent
+	private Key consumerId;
+	
+	@Persistent
+	private Key customerId;
+	
+	@Persistent
+	private Date paymentDate;
+	
+	@Persistent
+	private Double amount;
+	
+	@Persistent
+	private Double cashAmount;
+	
+	@Persistent
+	private Double ticketAmount;
+	
+	@Persistent
+	private Double restoAmount;
+
+	public Key getConsumerId() {
+		return consumerId;
+	}
+
+	public void setConsumerId(Key consumerId) {
+		this.consumerId = consumerId;
+	}
+
+	public Key getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Key customerId) {
+		this.customerId = customerId;
+	}
+
+	public Date getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(Date paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public Double getCashAmount() {
+		return cashAmount;
+	}
+
+	public void setCashAmount(Double cashAmount) {
+		this.cashAmount = cashAmount;
+	}
+
+	public Double getTicketAmount() {
+		return ticketAmount;
+	}
+
+	public void setTicketAmount(Double ticketAmount) {
+		this.ticketAmount = ticketAmount;
+	}
+
+	public Double getRestoAmount() {
+		return restoAmount;
+	}
+
+	public void setRestoAmount(Double restoAmount) {
+		this.restoAmount = restoAmount;
+	}
+
+	public Key getId() {
+		return id;
+	}
+	
+	
+
+	
+
+}
